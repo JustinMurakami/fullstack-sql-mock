0be8ad6809a48f7198bf52bbb98bf3d263011ebb
Justin Murakami <jmurakami84@gmail.com>
2020-09-20 12:21:52 -0700

    modified products to render in product list with correct information and image.

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 const Products = (props) => {
    return(
     <div className='product-list-entry'>
+      <img src={props.image}></img>
+      <h4>{props.item}</h4>
+   <p>Current Bid: {props.curr_bid}</p>
+      <p>Ends in {props.ends_in} Days.</p>
     </div>
   )
 }
