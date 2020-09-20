704e7da76a90d4ba2bbb3b9bbb85a6c32474309c
Justin Murakami <jmurakami84@gmail.com>
2020-09-20 11:41:36 -0700

    have all controllers and helpers working via testing in postman.

--- db/dbhelpers.js
+++ db/dbhelpers.js
@@ -3,7 +3,6 @@ const db = require('./');
 
 
 module.exports.getProductsHelper = (callback) => {
-  console.log('hello');
   let queryString = `SELECT * FROM products`
   db.query(queryString, (err, results) => {
     if (err) {
@@ -14,8 +13,36 @@ module.exports.getProductsHelper = (callback) => {
   })
 }
 
-module.exports.postProductsHelper = () => {}
+module.exports.postProductsHelper = (data, callback) => {
+  let queryString = `INSERT INTO products (item, min_cost, curr_bid, ends_in, image) VALUES ('${data.item}', ${data.min_cost}, 0, 3, '${data.image}');`;
+  db.query(queryString, (err, results) => {
+    if (err) {
+      callback(err)
+    } else {
+      callback(null, results);
+    }
+  })
+}
 
-module.exports.updateProductHelper = () => {}
+module.exports.updateProductHelper = (id, data, callback) => {
+  let queryString =`UPDATE products SET curr_bid=${data.curr_bid} WHERE id=${id};`;
+  db.query(queryString, (err, results) => {
+    if (err) {
+      callback(err)
+    } else {
+      callback(null, results)
+    }
+  })
+}
 
-module.exports.deleteProductHelper = () => {}
+module.exports.deleteProductHelper = (id, callback) => {
+  console.log('THIS IS ID', id);
+  let queryString = `DELETE FROM products WHERE id=${id};`;
+  db.query(queryString, (err, results) => {
+    if (err) {
+      callback(err)
+    } else {
+      callback(null, results)
+    }
+  })
+}
--- server/controller.js
+++ server/controller.js
@@ -5,7 +5,6 @@ const dbhelpers = require('../db/dbhelpers.js');
 
 const controller = {
   get: (req, res) => {
-    console.log('GET CONTROLLER!!!')
     dbhelpers.getProductsHelper((err, results) =>{
       if (err) {
         res.status(400).send('error getting products')
@@ -16,12 +15,35 @@ const controller = {
   },
 
   post: (req, res) => {
-
+    dbhelpers.postProductsHelper(req.body, (err, results) => {
+      if (err) {
+        res.status(400).send('error posting product')
+      } else {
+        res.status(200).send(results)
+      }
+    })
   },
 
-  put: (req, res) => {},
+  put: (req, res) => {
+    dbhelpers.updateProductHelper(req.params.id, req.body, (err, results) => {
+      if (err) {
+        res.status(400).send('error updating product')
+      } else {
+        res.status(200).send(results)
+      }
+    })
+  },
 
-  delete: (req, res) => {}
+  delete: (req, res) => {
+    console.log('THIS IS REQ', req);
+    dbhelpers.deleteProductHelper(req.params._id, (err, results) => {
+      if (err) {
+        res.status(400).send('error deleting product')
+      } else {
+        res.status(200).send(results)
+      }
+    })
+  }
 
 }
 
